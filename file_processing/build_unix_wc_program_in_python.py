4cf0cda22bbafafd2f878e1bb38400df690be529
somekindofpast <multandras@gmail.com>
2024-11-10 15:02:28 +0100

    modify: build_unix_wc_program_in_python.py

--- file_processing/build_unix_wc_program_in_python.py
+++ file_processing/build_unix_wc_program_in_python.py
@@ -1,14 +1,12 @@
 import os
 import urllib.request
-from typing import Tuple
-from collections import Counter
 
 # data provided
 tmp = os.getenv("TMP", "/tmp")
-harry_file = os.path.join(tmp, 'harry')
+drive_file = os.path.join(tmp, 'drive.txt')
 urllib.request.urlretrieve(
     'https://bites-data.s3.us-east-2.amazonaws.com/driving.py',
-    harry_file
+    drive_file
 )
 
 def wc(file_):
@@ -16,18 +14,13 @@ def wc(file_):
        lines/words/chars, and returns a string of these numbers + file, e.g.:
        3 12 60 /tmp/somefile
        (both tabs and spaces are allowed as separator)"""
-    text = open(file_, "r", newline='')
-    lines = text.read().splitlines()
-    text.close()
-    words = 0
-    chars = 0
-    for line in lines:
-        words += len(line.split())
-        chars += len(line)
-    return ' '.join([str(len(lines)), str(words), str(chars)])
+    with open(file_) as f:
+        file_name = f.name.split('\\')[-1]
+        content = f.read()
+    return ' '.join([str(len(content.splitlines())), str(len(content.split())), str(len(content)), file_name])
 
 
 if __name__ == '__main__':
-    print(wc(harry_file))
+    print(wc(drive_file))
 
 
